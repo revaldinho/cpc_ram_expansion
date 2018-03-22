f52f9ae66ea036c5cbef617a5ae437f8dd0e7afc
revaldinho <richevans@mac.com>
2018-03-22 20:36:07 +0000

    Move all components onto VDD3V3

--- pcb/cpc_ram_board.v
+++ pcb/cpc_ram_board.v
@@ -150,7 +150,7 @@ module cpc_ram_board ();
 	            .p29(RAMDIS),
 	            .tdo(TDO),
 	            .gnd3(VSS),
-	            .vccio(VDD),
+	            .vccio(VDD3V3),
 	            .p33(D7),
 	            .p34(D6),
 	            .p35(D5),
@@ -167,7 +167,7 @@ module cpc_ram_board ();
 
   // Alliance 512K x 8 SRAM - address pins wired to suit layout
   bs62lv4006  SRAM (
-                    .a18(HIADR4),  .vcc(VDD),
+                    .a18(HIADR4),  .vcc(VDD3V3),
                     .a16(HIADR2),  .a15(HIADR1),
                     .a14(HIADR0),  .a17(HIADR3),
                     .a12(A5),  .web(RAMWE_B),
@@ -186,8 +186,8 @@ module cpc_ram_board ();
                     );
 
    // Decoupling caps for CPLD and one for SRAM
-   cap100nf CAP100N_1 (.p0( VSS ), .p1( VDD ));
-   cap100nf CAP100N_2 (.p0( VSS ), .p1( VDD ));
+   cap100nf CAP100N_1 (.p0( VSS ), .p1( VDD3V3 ));
+   cap100nf CAP100N_2 (.p0( VSS ), .p1( VDD3V3 ));
    cap100nf CAP100N_3 (.p0( VSS ), .p1( VDD3V3 ));
 
 endmodule
