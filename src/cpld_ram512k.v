e68153244a45187a6d9ed6475ae43e8948b94f3d
revaldinho <richevans@mac.com>
2018-04-19 20:05:14 +0100

    Improvements/simplifications to CPLD code - working on 1st proto board with mini soak test

--- src/cpld_ram512k.v
+++ src/cpld_ram512k.v
@@ -25,74 +25,65 @@
  *
  */
 
-module cpld_ram512k(adr15, adr14,iorq_b,mreq_b,ramrd_b,busreset_b,reset_b,wr_b,rd_b,data,ramdis,ramcs_b,ramadrhi,ready, clk, ramoe_b, ramwe_b);
-    input           adr15;
-    input           adr14;  
-    input           iorq_b;
-    input           mreq_b;
-    input           ramrd_b;
-    input 	    reset_b;
-    input           wr_b;
-    input [7:0]     data;
-    input	    ready;
-    input 	    clk;
-    // unused inputs 
-    input           busreset_b;
-    input           rd_b;    
-
-    output	    ramdis;
-    output	    ramcs_b;
-    output [4:0]    ramadrhi;
-    output          ramoe_b;
-    output          ramwe_b;
-
-    reg [7:0]       ioreg_q;    
-    reg [5:0]       ramblock_q;
-    reg [4:0]	    ramadrhi_r;
-    reg		    notextram_r;
-    reg [7:0]       dataout_r;
-
-    reg             clken_lat_q;
-
-             
-
-  
-    // Create neg edge clock on write event 
-    wire wclk_b    = ! (clk||clken_lat_q); 
-
-    // Pass through's just in case ...
-    assign ramoe_b = ramrd_b;
-    assign ramwe_b = wr_b;
-
-    always @ ( * )
-        if ( clk )
-            clken_lat_q <= !(!iorq_b && !wr_b && !adr15 && data[6] && data[7]);
-    
-    always @ (negedge reset_b or posedge wclk_b )
-        if (!reset_b)
-            ramblock_q <= 5'b0;
-        else
-            ramblock_q <= data[5:0];
-
-    // Select RAM for all memory accesses with valid bank num
-    assign ramcs_b  = notextram_r | mreq_b ; 
-    assign ramdis   = ( !(notextram_r | mreq_b) ) ;    
-
-    assign ramadrhi = ramadrhi_r ;        
-    always @ (ramblock_q, adr15, adr14 )
-        case (ramblock_q[2:0])
-            3'b000: {notextram_r, ramadrhi_r} = { 1'b1, 5'bx };               
-            3'b001: {notextram_r, ramadrhi_r} = ( {adr15,adr14}==2'b11 ) ? {1'b0,ramblock_q[5:3],{1'b1,ramblock_q[0]}} : {1'b1, 5'bx} ; 
-            3'b010: {notextram_r, ramadrhi_r} = { 1'b0,ramblock_q[5:3],adr15,adr14} ;    
-            3'b011: {notextram_r, ramadrhi_r} = ( {adr15,adr14}==2'b11 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
-            3'b100: {notextram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
-            3'b101: {notextram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
-            3'b110: {notextram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
-            3'b111: {notextram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
-            default: {notextram_r, ramadrhi_r} = { 1'bx, 5'bx };
-        endcase
-    
+module cpld_ram512k(busreset_b,adr15,adr14,iorq_b,mreq_b,ramrd_b,reset_b,wr_b,rd_b,data,ramdis,ramcs_b,ramadrhi,ready, clk, ramoe_b, ramwe_b);
+   input            busreset_b;   
+   input            adr15;
+   input            adr14;
+   input            iorq_b;
+   input            mreq_b;
+   input            ramrd_b;
+   input 	    reset_b;
+   input            wr_b;
+   input            rd_b;    
+   input [7:0]      data;
+   input	    ready;
+   input 	    clk;
+   
+   output	    ramdis;
+   output	    ramcs_b;
+   output [4:0]     ramadrhi;
+   output           ramoe_b;
+   output           ramwe_b;
+   
+   reg [5:0]        ramblock_q;
+   reg [4:0]        ramadrhi_r;
+   reg		    ramcs_b_r;
+   
+   reg              clken_lat_qb;
+   
+   // Create clock on IO write event - clock low pulse will be suppressed if not an IOWR* event
+   // but if the pulse is allowed through use the trailing (rising) edge to capture data
+   wire             wclk    = (clk|clken_lat_qb); 
+   
+   // Combination of RAMCS and RAMRD determine whether RAM output is enabled
+   assign ramoe_b = ramrd_b;		
+   assign ramwe_b = wr_b | mreq_b;     
+   assign ramcs_b = ramcs_b_r;
+   assign ramdis  = !ramcs_b_r;    
+   assign ramadrhi = ramadrhi_r ;        
+   
+   always @ ( clk )
+     if ( clk )
+       clken_lat_qb <= !(!iorq_b && !wr_b && !adr15 && data[6] && data[7]);
+   
+   always @ (negedge reset_b or posedge wclk )
+     if (!reset_b)
+       ramblock_q <= 6'b0;
+     else
+       ramblock_q <= data[5:0];
+   
+   always @ (ramblock_q, adr15, adr14 )
+     case (ramblock_q[2:0])
+       3'b000: {ramcs_b_r, ramadrhi_r} = { 1'b1, 5'bx };               
+       3'b001: {ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b11 ) ? {1'b0,ramblock_q[5:3],{1'b1,ramblock_q[0]}} : {1'b1, 5'bx} ; 
+       3'b010: {ramcs_b_r, ramadrhi_r} = { 1'b0,ramblock_q[5:3],adr15,adr14} ;    
+       3'b011: {ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b11 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
+       3'b100: {ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
+       3'b101: {ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
+       3'b110: {ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
+       3'b111: {ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b0,ramblock_q[5:3],ramblock_q[1:0]} : {1'b1, 5'bx} ; 
+     endcase
+   
 endmodule
-
 
 
