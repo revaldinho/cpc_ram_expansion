0c17c7f407384d6b242f9a697b20b7ea30e995bf
revaldinho <richevans@mac.com>
2018-10-13 17:30:08 +0100

    Restore RC-2 code, update comments, synchronous resets

--- cpc_ram_expansion/src/cpld_ram512k_v110.v
+++ cpc_ram_expansion/src/cpld_ram512k_v110.v
@@ -35,39 +35,30 @@
  * BUF mybuf (.I(input_a_reg),.O(comb));
  */
 
-
-// Evaluate benefits of having full shadow mode only in synthesis
-//`define FULL_SHADOW_ONLY 1
-`define ENABLE_TURBO 1
-
 module cpld_ram512k_v110(
-  input       rfsh_b,
-  inout       adr15,
-  inout       adr15_aux, 
-  input       adr14,
-  input       adr8, 
-  input       iorq_b,
-  input       mreq_b,
-  input       ramrd_b,
-  input       reset_b,
-  input       wr_b,
-  inout       rd_b,
-  inout       rd_b_aux, 
-  input [7:0] data,
-`ifdef ENABLE_TURBO
-  inout       ready,                         
-`else                         
-  input       ready,
-`endif                         
-  input       clk,
-  input       m1_b,
-  input [1:0] dip,
+  input        rfsh_b,
+  inout        adr15,
+  inout        adr15_aux, 
+  input        adr14,
+  input        adr8, 
+  input        iorq_b,
+  input        mreq_b,
+  input        ramrd_b,
+  input        reset_b,
+  input        wr_b,
+  inout        rd_b,
+  inout        rd_b_aux, 
+  input [7:0]  data,
+  inout        ready,
+  input        clk,
+  input        m1_b,
+  input [1:0]  dip,
     
-  output      ramdis,
-  output      ramcs_b,
-  inout [4:0] ramadrhi, // bits 4,3 also connected to DIP switches 2,1 resp and read on startup
-  output      ramoe_b,
-  output      ramwe_b
+  output       ramdis,
+  output       ramcs_b,
+  inout [4:0]  ramadrhi, // bits 4,3 also connected to DIP switches 2,1 resp and read on startup
+  output       ramoe_b,
+  output       ramwe_b
 );
   
   reg [5:0]        ramblock_q;
@@ -83,56 +74,37 @@ module cpld_ram512k_v110(
   reg              mreq_b_q, mreq_b_f_q;
   wire             mwr_cyc_d;  
   wire             adr15_overdrive_w;
-  wire             turbo_mode;
-  wire             shadow_mode;
-  wire             overdrive_mode;
-  wire             full_shadow;
-  wire [2:0]       shadow_bank;
 
-/* 
- * D D D D  NB DIP Switches labelled 1-4 on the physical component on the board, but the
- * I I I I  DIP numbers run from 0-3 in the code.
- * P P P P
- * 1 2 3 4  Function settings                           Total Exp/SiDisc Compatibility
- * -----------------------------------------------------------------------------------------------------
- * 0 0 0 0  OD OFF, Shadow OFF, turbo OFF               512KB 256/256    6128/Plus
- *                                                            
- * 0 1 0 0  OD OFF, Shadow FULL, Bank LOW, turbo OFF    448KB 192/256    6128/Plus w. SiDisk [1]
- * 0 1 0 1  OD OFF, Shadow FULL, Bank HIGH, turbo OFF   448KB 448/0      6128/Plus [1]
- * 0 1 1 0  OD OFF, Shadow FULL, Bank LOW, turbo ON     448KB 192/256    6128/Plus w. SiDisk [1,2]
- * 0 1 1 1  OD OFF, Shadow FULL, Bank HIGH, turbo ON    448KB 448/0      6128/Plus [1,2]
- *                                                                         
- * 1 0 0 0  OD ON, Shadow OFF, turbo OFF                512KB 256/256    464 DK'T
- *                                                                         
- * 1 0 1 0  OD ON, Shadow PARTIAL, Bank LOW, turbo OFF  448KB 192/256    464 C3 (FutureOS) w. SiDisk
- * 1 0 1 1  OD ON, Shadow PARTIAL, Bank HIGH, turbo OFF 448KB 448/0      464 C3 (FutureOS) 
- *                                                                         
- * 1 1 0 0  OD ON, Shadow FULL, Bank LOW, turbo OFF     448KB 192/256    464 C3 (FutureOS) w. SiDisk
- * 1 1 0 1  OD ON, Shadow FULL, Bank HIGH, turbo OFF    448KB 448/0      464 C3 (FutureOS)
- * -----------------------------------------------------------------------------------------------------
- * Experimental code only - not enabled 
- * 1 1 1 0  OD ON, Shadow FULL, Bank LOW, turbo ON      448KB 192/256    464 C3 (FutureOS) w. SiDisk [2]
- * 1 1 1 1  OD ON, Shadow FULL, Bank HIGH, turbo ON     448KB 448/0      464 C3 (FutureOS) [2]
- * -----------------------------------------------------------------------------------------------------
- * 
- * [1] FULL shadow mode still not seen working on 6128
- * [2] Turbo mode needs to be tested for limitations - already know that
- *     use in all modes can cause video problems in CP/M and Phortem even
- *     when mode 3 specifically disabled.
- */
+  // NBDIP switches are numbered 1-4 on the physical component, but 0-3 in Verilog code
+  // 
+  // 1 2 3 4| Note                                                               | Supported Video Modes (Notes)
+  // -------+--------------------------------------------------------------------+------------------------------
+  //        | Recommended Video Modes                                            |
+  // -------+--------------------------------------------------------------------+------------------------------
+  // 0 0 0 0| Only valid 6128 or 464+, 6128+ setting                             | 0-7
+  // -------+--------------------------------------------------------------------+------------------------------  
+  // 1 1 0 0| 464 overdrive mode, partial shadow RAM, shadow bank number 3'b011  | 0-7
+  // 1 1 0 1| 464 overdrive mode, partial shadow RAM, shadow bank number 3'b111  | 0-7
+  // 1 1 1 0| 464 overdrive mode, full shadow RAM, shadow bank number 3'b011     | 0-7
+  // 1 1 1 1| 464 overdrive mode, full shadow RAM, shadow bank number 3'b111     | 0-7
+  // -------+--------------------------------------------------------------------+------------------------------
+  //        | Limited Video Modes                                                |
+  // -------+--------------------------------------------------------------------+------------------------------
+  // 0 1 1 0| 464 full shadow RAM, no overdrive, shadow bank number 3'b011       | 0,4-7(1)
+  // 0 1 1 1| 464 full shadow RAM, no overdrive, shadow bank number 3'b111       | 0,4-7(1)
+  // 1 0 0 0| 464 overdrive mode, no shadow RAM                                  | 0,1,2,3(2),4-7    
+  // -------+--------------------------------------------------------------------+------------------------------
+  // Notes
+  // (1) - no screen protection, visible interference whenever writing to expansion memory overlaid on screen memory
+  // (2) - remapping of _internal_ memory in mode C3 done but subject to errors when foreground ROM is selected
+  //
 
-  assign overdrive_mode = dip[0];                  // ie DIP SW 1 
-  assign shadow_mode = dip[1] | dip_q[2];        // ie DIP SW 2 OR DIP SW 3
-  assign full_shadow = dip[1];                   // ie DIP SW 2
-  assign shadow_bank = { dip_q[3], 2'b11 } ;       // ie DIP SW 4
-  assign turbo_mode  = dip[1] & dip_q[2];        // ie DIP SW 2 AND DIP SW 3
-  
-  // Should be able to ignore waits on all reads from expansion RAM but CP/M shows some visible pixel
-  // corruption in mode2 which can be eliminated by this additional term:  ({adr14,ramblock_q[2:0]}!=4'b1010)
-`ifdef ENABLE_TURBO  
-  assign ready =  (turbo_mode & ((!(|ramblock_q[1:0])|ramblock_q[2]) & !mwr_cyc_q & !ramrd_b & !mreq_b) ) ? 1'b1 : 1'bz;
-`endif
-  
+  wire        overdrive_mode = dip[0];                     // AKA 464 mode [1] or 6128 mode [0]
+  wire        shadow_mode = dip[1];                        // Only valid in overdrive mode for 464
+  // Latching these two DIP switches requires the CPC to be powered down/up rather than just a ctrl-shift-esc reset
+  wire        full_shadow = dip_q[2] & shadow_mode;        // Full shadow mode [1] or partial mode [0] for 464
+  wire [2:0]  shadow_bank = {dip_q[3], 2'b11};             // use 3'b111 or 3'b011 for shadow bank for 464
+
   // Create negedge clock on IO write event - clock low pulse will be suppressed if not an IOWR* event
   // but if the pulse is allowed through use the trailing (rising) edge to capture data
   wire             wclk    = !(clk|clken_lat_qb); 
@@ -170,17 +142,11 @@ module cpld_ram512k_v110(
   assign adr15_overdrive_w   =  overdrive_mode & mode3_q & adr14 & rfsh_b & ((shadow_mode) ? (mwr_cyc_q|mwr_cyc_d): !mreq_b) ;
   assign { adr15, adr15_aux} = (adr15_overdrive_w  ) ? 2'b11 : 2'bzz; 
 
-`ifdef FULL_SHADOW_ONLY
-  // Never, ever use internal RAM for reads in full shadow mode  (but allow it in overdrive only)
-  assign ramdis = (shadow_mode) ? 1'b1 :  !ramcs_b_r ;  
-  // In full shadow mode SRAM is always enabled for all real memory accesses but dont clash with ROM access (ramrd_b will control oe_b)
-  assign ramcs_b = mreq_b | !rfsh_b ;  
-`else  
-  // Never, ever use internal RAM for reads in full shadow mode  
-  assign ramdis = (full_shadow) ? 1'b1 :  !ramcs_b_r ;  
+  // Never, ever use internal RAM for reads in full shadow mode
+  assign ramdis = (full_shadow) ? 1'b1 :  !ramcs_b_r ;
+  
   // In full shadow mode SRAM is always enabled for all real memory accesses but dont clash with ROM access (ramrd_b will control oe_b)
   assign ramcs_b = !( !ramcs_b_r | full_shadow) | mreq_b | !rfsh_b ;
-`endif
 
   always @ (posedge clk)
     if ( !reset_b )
@@ -242,22 +208,6 @@ module cpld_ram512k_v110(
     if ( shadow_mode )
       // FULL SHADOW MODE    - all CPU read accesses come from external memory (ramcs_b_r computed here is ignored)            
       // PARTIAL SHADOW MODE - all CPU write accesses go to shadow memory but only shadow block 3 is ever read in mode C3 at bank 0x4000 (remapped to 0xC000)
-`ifdef FULL_SHADOW_ONLY
-      // ramcs_b_r never used in this mode
-      begin
-        ramcs_b_r = 1'bx;        
-	case (ramblock_q[2:0])
-	  3'b000: {exp_ram_r, ramadrhi_r} = { 1'b0, shadow_bank, adr15,adr14 };
-	  3'b001: {exp_ram_r, ramadrhi_r} = ( {adr15,adr14}==2'b11 ) ? {1'b1, ramblock_q[5:3],2'b11} : { 1'b0, shadow_bank, adr15,adr14 };
-	  3'b010: {exp_ram_r, ramadrhi_r} = { 1'b1, ramblock_q[5:3], adr15,adr14} ; 
-	  3'b011: {exp_ram_r, ramadrhi_r} = ( {adr15_q,adr14}==2'b11 ) ? {1'b1,ramblock_q[5:3],2'b11} : ({adr15_q,adr14}==2'b01) ? {1'b0,shadow_bank,2'b11} : { 1'b0, shadow_bank, adr15,adr14 };
-	  3'b100: {exp_ram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b1,ramblock_q[5:3],2'b00} : { 1'b0, shadow_bank, adr15, adr14 };              
-	  3'b101: {exp_ram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b1,ramblock_q[5:3],2'b01} : { 1'b0, shadow_bank, adr15, adr14 };              
-	  3'b110: {exp_ram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b1,ramblock_q[5:3],2'b10} : { 1'b0, shadow_bank, adr15, adr14 };              
-	  3'b111: {exp_ram_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {1'b1,ramblock_q[5:3],2'b11} : { 1'b0, shadow_bank, adr15, adr14 };
-	endcase // case (ramblock_q[2:0])
-      end
-`else      
       case (ramblock_q[2:0])
  	3'b000: {exp_ram_r, ramcs_b_r, ramadrhi_r} = { 1'b0, !mwr_cyc_q , shadow_bank, adr15,adr14 };
  	3'b001: {exp_ram_r, ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b11 ) ? {2'b10, ramblock_q[5:3],2'b11} : { 1'b0, !mwr_cyc_q , shadow_bank, adr15,adr14 };
@@ -267,8 +217,7 @@ module cpld_ram512k_v110(
  	3'b101: {exp_ram_r, ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {2'b10,ramblock_q[5:3],2'b01} : { 1'b0, !mwr_cyc_q , shadow_bank, adr15, adr14 };              
  	3'b110: {exp_ram_r, ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {2'b10,ramblock_q[5:3],2'b10} : { 1'b0, !mwr_cyc_q , shadow_bank, adr15, adr14 };              
  	3'b111: {exp_ram_r, ramcs_b_r, ramadrhi_r} = ( {adr15,adr14}==2'b01 ) ? {2'b10,ramblock_q[5:3],2'b11} : { 1'b0, !mwr_cyc_q , shadow_bank, adr15, adr14 };
-      endcase // case (ramblock_q[2:0])
-`endif    
+      endcase 
     else
       // 6128 mode. In 464 mode (ie overdrive ON but no shadow memory) means that C3 is not fully supported for FutureOS etc, but other modes are ok
       case (ramblock_q[2:0])
