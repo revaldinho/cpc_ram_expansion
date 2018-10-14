7de655333d871b6f8842e640b6f0d867c1ffb1d7
revaldinho <richevans@mac.com>
2018-10-14 21:57:18 +0100

    First check-in of z80tube code

--- cpc_6502/src/z80tube.v
+++ cpc_6502/src/z80tube.v
@@ -1,27 +1,27 @@
 module z80tube(
                // Host
-               input CLK,
-               input [15:0] ADR,
-               input RD_B,
-               input WR_B,               
-               input IOREQ_B,
-               input M1_B,                              
-               input MREQ_B,
-               input WAIT_B,
-               
-               inout [7:0] DATA,
-               inout NMI_B,               
-               inout INT_B,
+               input         CLK,
+               input [15:0]  ADR,
+               input         RD_B,
+               input         WR_B, 
+               input         IOREQ_B,
+               input         M1_B, 
+               input         MREQ_B,
+               input         WAIT_B,
+               input         RESET_B,
+               inout [7:0]   DATA,
+               inout         NMI_B, 
+               inout         INT_B,
                
                // Tube	
-               input TUBE_INT_B,
+               input         TUBE_INT_B,
                
-               inout [7:0] TUBE_DATA,
+               inout [7:0]   TUBE_DATA,
                
                output [15:0] TUBE_ADR,
-               output TUBE_RNW_B,
-               output TUBE_PHI2,
-               output TUBE_CS_B
+               output        TUBE_RNW_B,
+               output        TUBE_PHI2,
+               output        TUBE_CS_B
                );	
 
 //      IDLE  .  S0     .   S0    .   S1    .    S2   .  IDLE  (negedge state)
@@ -56,51 +56,92 @@ module z80tube(
 //
     
     
-parameter IDLE=0,S0=1,S1=2,S2=3,S3=4;
-
-reg	posen_q;
-reg	negen_f_q;
-reg [2:0] state_f_q;
-reg [2:0] state_d;    
-reg     wr_qb;
-reg     wait_f_q;	
-
-wire   chip_select ;
-
-assign chip_select =  ( ADR[15:4] = 12'hFA8 ) ; 
-
-// TUBE
-assign TUBE_CS_B = !(!IOREQ_B & chip_select);
-assign TUBE_PHI2 = negen_f_q | posen_q;
-assign TUBE_ADR = ADR;
-assign TUBE_RNW_B = IOREQ_B | WR_B;
-assign TUBE_DATA = ( !wr_qb & ((state_f_q=S1)|(state_f_q=S2)) & posen_q ) ? DATA : 8'bz;
-
-// MASTER - drive databus on reads
-assign DATA = ( !IOREQ_B & chip_select & wr_qb & ((state_f_q=S1) | (state_f_q=S2)) & posen_q)? TUBE_DATA : 8'bz;
-assign NMI = 1'bz;
-assign INT = 1'bz;
-    
-always @ ( negedge CLK )
-   case (state_f_q)
-       IDLE:  state_d = ( !IOREQ_B) ? S0 : IDLE ;
-       S0  :  state_d = (!WAIT_B) ? S1 : S0;
-       S1  :  state_d = S2;
-       S2  :  state_d = IDLE;
-       default: state_d = IDLE;
-   endcase	
-           
- // PHI2 high must be glitchless - create it from just two FFs on clk/clkbar
- always @ (negedge clk ) begin
-     if ( state_f_q=S0 )
-         negen_f_q = 1'b1;
-     else if ( state_f_q=S1)
-         negen_f_q = 1'b0;                        
- end
+  parameter IDLE=0,S0=1,S1=2,S2=3,S3=4;
+  
+  reg                 posen_q;
+  reg                 negen_f_q;
+  reg [2:0]           state_f_q;
+  reg [2:0]           state_d;    
+  reg                 wr_b_q;
+  reg                 wait_f_q;	
+  reg [7:0]           status_q;
+  reg [7:0]           data_r;
+  reg                 data_en_r;
+  reg                 clken_lat_b_q;
+  reg [1:0]           reset_b_q;
+   
+  wire                tube_chip_select ;
+  wire                status_reg_select ;  
+  wire                wclk;
+  wire                reset_b_w;
+               
+  assign status_reg_select    =  ( ADR == 16'hFFF9 ) ; 
+  assign tube_chip_select =  ( ADR[15:4] == 12'hFA8 ) ; 
+  
+  // TUBE
+  assign TUBE_CS_B = !(!IOREQ_B & tube_chip_select);
+  assign TUBE_PHI2 = negen_f_q | posen_q;
+  assign TUBE_ADR = ADR;
+  assign TUBE_RNW_B = IOREQ_B | WR_B;
+  assign TUBE_DATA = ( !wr_b_q & ((state_f_q==S1)|(state_f_q==S2)) & posen_q ) ? DATA : 8'bz;
+  
+  // MASTER - drive databus on reads
+  assign DATA = ( data_en_r ) ? data_r : 8'bzzzzzzzz ;
+  assign NMI = 1'bz;
+  assign INT = 1'bz;
+  
+  // Synchronized reset release to posedge of clock
+  assign reset_b_w = RESET_B & reset_b_q[0];
  
- always @ (posedge clk ) begin
-     posen_q <= negen_f_q;
-     wr_qb   = wr_b;     
- end
+  always @ ( negedge CLK )
+    case (state_f_q)
+      IDLE:  state_d = ( !IOREQ_B) ? S0 : IDLE ;
+      S0  :  state_d = (!WAIT_B) ? S1 : S0;
+      S1  :  state_d = S2;
+      S2  :  state_d = IDLE;
+      default: state_d = IDLE;
+    endcase	
+
+  // PHI2 high must be glitchless - create it from just two FFs on clk/clkbar
+  always @ (negedge CLK ) begin
+    if ( state_f_q==S0 )
+      negen_f_q = 1'b1;
+    else if ( state_f_q==S1)
+      negen_f_q = 1'b0;                        
+  end
+  
+  always @ (posedge CLK ) begin
+    posen_q <= negen_f_q;
+    wr_b_q   = WR_B;
+    reset_b_q <= {RESET_B, reset_b_q[1]};    
+  end
+
+  // Data mux to service IO Reads by the CPC Z80 Host
+  always @ ( * ) begin
+    if ( !IOREQ_B & !RD_B) begin
+      if ( status_reg_select )
+        { data_en_r, data_r } = { 1'b1, status_q }   ; 
+      else if ( tube_chip_select )
+        { data_en_r, data_r } = { 1'b1, TUBE_DATA }   ;
+      else
+        { data_en_r, data_r } = { 1'b0, 8'bx } ;
+    end  
+    else
+        { data_en_r, data_r } = { 1'b0, 8'bx } ;              
+  end // always @ ( * )  
     
+  // Create negedge clock on IO write event - clock low pulse will be suppressed if not an IOWR* event
+  // but if the pulse is allowed through use the trailing (rising) edge to capture data
+  assign wclk    = !(CLK|clken_lat_b_q);
+           
+  always @ ( * )
+    if ( CLK ) 
+      clken_lat_b_q <= !(!IOREQ_B && !WR_B && status_reg_select );
+
+  always @ (posedge wclk )
+    if (!reset_b_w)
+      status_q <= 8'b0;
+    else
+      status_q <= DATA[7:0];
+  
 endmodule
