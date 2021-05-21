bfea74c2259161d7b8377080e854ab2a4be23530
revaldinho <richevans@mac.com>
2021-05-21 20:00:33 +0100

    revert PLCC84 experiments to match PLCC44 code

--- cpc_ram_expansion1mb/src/cpld_ram1m_plcc84.v
+++ cpc_ram_expansion1mb/src/cpld_ram1m_plcc84.v
@@ -60,22 +60,22 @@
  *
  */
 
-//`define CPC6128_ONLY 1
-//`define EXT_RAMRD 1
-//`define DISABLE_RESET_RESYNC 1
-//`define FULL_SHADOW_ONLY     1
-
-`define FIT_XC9536 1
-
-`ifdef CPC6128_ONLY
-  `define EXT_RAMRD 1
-`endif
-
- `ifdef FIT_XC9536
-  // Disable resynchronisation of reset
-  `define DISABLE_RESET_RESYNC 1
-  `define FULL_SHADOW_ONLY     1
-`endif
+// //`define CPC6128_ONLY 1
+// //`define EXT_RAMRD 1
+// //`define DISABLE_RESET_RESYNC 1
+// //`define FULL_SHADOW_ONLY     1
+// 
+// //`define FIT_XC9536 1
+// 
+// `ifdef CPC6128_ONLY
+//   `define EXT_RAMRD 1
+// `endif
+// 
+//  `ifdef FIT_XC9536
+//   // Disable resynchronisation of reset
+//   `define DISABLE_RESET_RESYNC 1
+//   `define FULL_SHADOW_ONLY     1
+// `endif
 
 
 module cpld_ram1m_plcc84(
@@ -116,10 +116,9 @@ module cpld_ram1m_plcc84(
 
   parameter   shadow_bank= 4'b0111;  // Shadow bank always high bank in 7FFE RAM (SRAM0)
 
-  reg              yarek_bank_sel_r;  
   reg [6:0]        ramblock_q;
   reg [5:0]        ramadrhi_r;
-  reg              cardsel_q;
+  wire             cardsel_w;
   wire             adr15_mx_w;
   reg              mode3_overdrive_q;
   reg              mwr_cyc_q;
@@ -190,26 +189,13 @@ module cpld_ram1m_plcc84(
 
   assign ram64kb_mode  = !dip[2] & dip[3];
   assign ram1mb_mode   = dip[2] & dip[3];
-
+  assign cardsel_w     = dip[2] | dip[3];
   
   // Dont drive address outputs during reset due to overlay of DIP switches
   assign ramadrhi =  ( !reset_b_w ) ? 5'bzzzzz : ramadrhi_r[4:0];
   assign ram_ctrl_select_w = (!ioreq_b && !wr_b && !adr15 && data[6] && data[7] );
   assign rom_ctrl_select_w = (!ioreq_b && !wr_b && !adr15 && !data[6] && data[7] );
 
-
-always @ ( * ) begin
-  // 1 - chose with Adr[8] -> fail  (tried with inverted adr[8] - fail)
-  //yarek_bank_sel_r = !adr[8];
-  // 2 - chose with Adr[10] -> fail (tried with inverted adr[10] - fail)
-  //yarek_bank_sel_r = !adr[10];
-  // 3 - chose with Adr[9] -> fail
-  //yarek_bank_sel_r = adr[9];
-  // 4 - default to 0x7Fxx bank unless 0x78xx is chosen 
-  yarek_bank_sel_r = (adr[10:8]==3'b110)?1'b0: 1'b1;
-  end
-
-  
 `ifdef DISABLE_RESET_RESYNC
   assign reset_b_w = busreset_b;
 `else
@@ -218,21 +204,22 @@ always @ ( * ) begin
 
 `ifndef EXT_RAMRD
   // NB mode 3 DK'T 0x4000 -> remapped to 0xC000, so allow for this when
-  // creating our internal version of ramrd_r
+  // creating our internal version of ramrd_b_r
   always @ ( * ) begin
     int_ramrd_b_r = 1'b1 ; 	 // default disable RAM accesses
     if ( rfsh_b & !mreq_b & !rd_b)
-      if ( urom_disable_q & ( {adr15_q,adr[14]} == 2'b11 ))
+      if ( adr15_mx_w != adr[14] )
+        int_ramrd_b_r = 1'b0; // RAM in range 0x4000 - 0xBFFF never overlapped by ROM
+                            // and this is true even if we overdrive adr15.
+      else if ( urom_disable_q & ( {adr15_q,adr[14]} == 2'b11 ))
         int_ramrd_b_r = 1'b0; // RAM read in 0xC000 - 0xFFFF only if UROM disabled
       else if ( lrom_disable_q & ( {adr15_q,adr[14]} == 2'b00 ))
         int_ramrd_b_r = 1'b0; // RAM read in 0x0000 - 0x3FFF only if LROM disabled
-      else
-        int_ramrd_b_r = 1'b0;
   end
 `endif //  `ifndef EXT_RAMRD
 
   // Remember that wr_b is overdrive for first high phase of clock for M4 compatibility so don't write ;
-  assign ramwe_b = ! ( !wr_b & mwr_cyc_f_q );
+  assign ramwe_b = ! ( !wr_b & mwr_cyc_q & mwr_cyc_f_q );
   assign ramoe_b = ramrd_b_w ;
 
   /* Memory Data Access
@@ -267,7 +254,7 @@ always @ ( * ) begin
   assign adr15_mx_w = (mreq_b) ? adr15: adr15_q;
 
   // Never, ever use internal RAM for reads in full shadow mode
-  assign ramdis = (cardsel_q & (full_shadow | !ramcs_b_r)) ? 1'b1 : 1'bz;
+  assign ramdis = (cardsel_w & (full_shadow | !ramcs_b_r)) ? 1'b1 : 1'bz;
 
   // Alternative
   // - drive ramdis and romdis
@@ -276,16 +263,16 @@ always @ ( * ) begin
   // - full shadow mode not addressed here
   //
   // assign adr15_overdrive_w = mode3_overdrive_q & adr14 & !adr15_q & !mreq_b ;
-  // assign ramromdis = (cardsel_q & !ramcs_b_r & int_ramrd_r) ? 1'b1 : 1'bz;
+  // assign ramromdis = (cardsel_w & !ramcs_b_r & int_ramrd_r) ? 1'b1 : 1'bz;
 
   // Low RAM is enabled for all (base memory) shadow writes and when selected by 0x7Exx
 `ifdef FULL_SHADOW_ONLY
-  assign ramcs0_b =  !cardsel_q | !((!ramadrhi_r[5] & exp_ram_r) | (full_shadow & !exp_ram_r)) | mreq_b | !rfsh_b;
+  assign ramcs0_b =  !cardsel_w | !((!ramadrhi_r[5] & exp_ram_r) | (full_shadow & !exp_ram_r)) | mreq_b | !rfsh_b;
 `else
-  assign ramcs0_b =  !cardsel_q | !((!ramadrhi_r[5] & exp_ram_r) | (full_shadow & !exp_ram_r) | (shadow_mode & !exp_ram_r & !ramcs_b_r)) | mreq_b | !rfsh_b;
+  assign ramcs0_b =  !cardsel_w | !((!ramadrhi_r[5] & exp_ram_r) | (full_shadow & !exp_ram_r) | (shadow_mode & !exp_ram_r & !ramcs_b_r)) | mreq_b | !rfsh_b;
 `endif
   // High RAM is enabled only for expansion RAM accesses to 0x7Fxx
-  assign ramcs1_b = (!cardsel_q) | !(ramadrhi_r[5] & exp_ram_r) | mreq_b | !rfsh_b ;
+  assign ramcs1_b = (!cardsel_w) | !(ramadrhi_r[5] & exp_ram_r) | mreq_b | !rfsh_b ;
 
   always @ (posedge clk)
     if ( mwr_cyc_d )
@@ -326,38 +313,34 @@ always @ ( * ) begin
     if (!reset_b_w) begin
       ramblock_q <= 7'b0;
       mode3_overdrive_q <= 1'b0;
-      urom_disable_q <= 1'b0;
-      lrom_disable_q <= 1'b0;
-      cardsel_q <= 1'b0;
     end
     else begin
        if ( ram_ctrl_select_w ) begin
-         // Trial run - disable card in 1MB mode if not accessed as 0x7E or 0x7F - ie other ranges unimplemented
-         // Adding this term makes the 6128 64K expansion appear in the address space to some test programs
-         cardsel_q <= (dip[2] | dip[3]) & (!ram1mb_mode | (adr[11:9]==3'b111));
-         // Lower nybble of upper address is used to select sets of 8 banks in extended scheme
-         // In 1MB mode make only 0xXF and 0xXE valid.
          if (ram64kb_mode)
            // 64KB mode, selects always bank 0 in upper RAM (and doesn't clash with shadow bank)
            ramblock_q <= {4'b1000, data[2:0]} ;
-         else if (ram1mb_mode) begin
-           // 1MB mode uses given bank in upper (0x7Fxx) or lower (0x7Exx) RAM, but if bank selected is lower 0b111
+         else if (ram1mb_mode)
+           // 1MB mode uses given bank in upper (0x7FFF) or lower (0x7FFE) RAM, but if bank selected is lower 0b111
            // (which is used for shadow RAM) then must alias that to the next lower bank 0b110
-           if ( shadow_mode & {yarek_bank_sel_r,data[5:3]}==shadow_bank )
-             ramblock_q <= {yarek_bank_sel_r,data[5:4], 1'b0, data[2:0]};
+           if ( shadow_mode & {adr[8],data[5:3]}==shadow_bank )
+             ramblock_q <= {adr[8],data[5:4], 1'b0, data[2:0]};
            else
-             ramblock_q <= {yarek_bank_sel_r,data[5:0]};
-         end         
+             ramblock_q <= {adr[8],data[5:0]};
          else
            // 512KB mode selects always upper RAM (and doesn't clash with shadow bank)
            ramblock_q <= {1'b1,data[5:0]};
          mode3_overdrive_q <= overdrive_mode & (data[2:0] == 3'b011);
        end
-       else if ( rom_ctrl_select_w ) begin
-          { urom_disable_q, lrom_disable_q } <= data[3:2];
-       end
     end
 
+`ifndef EXT_RAMRD  
+  always @ (negedge clk or negedge reset_b_w)
+    if (!reset_b_w)
+      { urom_disable_q, lrom_disable_q}  <= 2'b00;
+    else if ( rom_ctrl_select_w )
+      { urom_disable_q, lrom_disable_q } <= data[3:2];
+`endif
+  
   always @ ( * ) begin
     if ( shadow_mode )
       // FULL SHADOW MODE    - all CPU read accesses come from external memory (ramcs_b_r computed here is ignored)
